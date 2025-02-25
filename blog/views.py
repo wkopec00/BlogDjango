0fcf0442a00b362ab9ebf870cb37ef2e89c26a4c
Wojtek <wojciechkopec@op.pl>
2025-02-25 19:14:20 +0100

    git init, strona główna

---
+++ blog/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def starting_page(request):
+    return render(request, 'blog/index.html')
+
+
+def posts(request):
+    pass
+
+
+def post(request):
+    pass
